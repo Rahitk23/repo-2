46a02b78b0ee0a3d46923c2b5a7c765315f75f7f
Richa Kathuria <rk.richakathuria@gmail.com>
2024-02-04 12:57:46 +0530

    Added code!

--- src/main/java/feb04/StudentMain.java
+++ src/main/java/feb04/StudentMain.java
@@ -5,32 +5,24 @@ import java.util.Collections;
 
 public class StudentMain {
     public static void main(String[] args) {
-        ArrayList<Student> studentslist=new ArrayList<>();
-        studentslist.add(new Student("Rahit",34));
-        studentslist.add(new Student("Richa",56));
-        studentslist.add(new Student("Zen",23));
+        ArrayList<Student> studentslist = new ArrayList<>();
+        studentslist.add(new Student("Rahit", 34));
+        studentslist.add(new Student("Richa", 56));
+        studentslist.add(new Student("Zen", 23));
 
-        studentslist.add(new Student("Amita",23));
+        studentslist.add(new Student("Amita", 23));
 
         System.out.println("Unsorted list: ");
-        for (Student student:
-             studentslist) {
-            System.out.print(student+" ");
-        }
-        Collections.sort(studentslist,new CompareStudentRollNo());
-//        System.out.println();
-//        System.out.println("After list sorted by rollno:");
-//        for (Student student:
-//                studentslist) {
-//            System.out.print(student+" ");
-//        }
+        System.out.println(studentslist);
 
-        Collections.sort(studentslist,new CompareStudentName());
+        Collections.sort(studentslist, new CompareStudentRollNo());
+        System.out.println();
+        System.out.println("After list sorted by rollno:");
+        System.out.println(studentslist);
+
+        Collections.sort(studentslist, new CompareStudentName());
         System.out.println();
         System.out.println("After list sorted by name:");
-        for (Student student:
-                studentslist) {
-            System.out.print(student+" ");
-        }
-    }
+        System.out.println(studentslist);
+   }
 }
